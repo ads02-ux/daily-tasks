0b451433458fe9984fbc365f2f3517150d957704
ads02-ux <ads02@richbiotech.com>
2026-09-25 19:03:58 +0700

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBltS6Pdey9e_b15JsMm6k1vdxS4bsBXj4",
+  authDomain: "richbio-daily-tasks.firebaseapp.com",
+  projectId: "richbio-daily-tasks",
+  storageBucket: "richbio-daily-tasks.firebasestorage.app",
+  messagingSenderId: "797336659859",
+  appId: "1:797336659859:web:09ceefe5fffef69e2daa66"
+};
